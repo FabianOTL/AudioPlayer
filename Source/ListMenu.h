675c3d5431c136adf8f0598e434b147f81807127
FabianOTL <otlacanfabian@gmail.com>
2026-09-11 15:08:30 +0300

    Playlist labels

--- Source/ListMenu.h
+++ Source/ListMenu.h
@@ -25,48 +25,66 @@ public:
         // Button to add a new playlist 
         juce::Image newPlaylistImage = juce::ImageFileFormat::loadFrom(juce::File(projectDir + "/Resources/add.png"));
 
-        newPlaylistButton.setImages(
+        auto newPlaylistButton = std::make_unique<juce::ImageButton>("Button");
+        newPlaylistButton->setImages(
         false,true,false,
         newPlaylistImage, 1.0f, juce::Colours::transparentWhite,
         newPlaylistImage, 1.0f, juce::Colours::transparentWhite,
         newPlaylistImage, 1.0f, juce::Colours::transparentWhite);
 
-        newPlaylistButton.onClick = [this] () {
+        newPlaylistButton->onClick = [this] () {
             changeState(MenuState::Add);
         };
 
-        addAndMakeVisible(&newPlaylistButton);
+        addAndMakeVisible(newPlaylistButton.get());
+        playlistButtons.push_back(std::move(newPlaylistButton));
     }
 
     ~ListMenu() override
     {
+
     }
 
     void paint (juce::Graphics& g) override
     {
+
     }
 
     void resized() override
     {
         const unsigned short size = 85, gap = 25;
-        auto area = getLocalBounds(); area.removeFromTop(gap);
-        auto row = area.removeFromTop(size); row.removeFromLeft(gap);
-        int count = 1;
-        newPlaylistButton.setBounds(row.removeFromLeft(size));
+        auto area = getLocalBounds();
+        auto row = area.removeFromTop(gap);
+        auto labelRow = area;
+        int count = 0;
    
         for(int i=0;i<playlistButtons.size();i++)
         {
-            if(count%5 == 0) {count = 0; area.removeFromTop(gap); row = area.removeFromTop(size); }
-            count++;
+            if(count%5 == 0) {count = 0; row = area.removeFromTop(size); labelRow = area.removeFromTop(gap); }
+                count++;
 
             row.removeFromLeft(gap);
+            labelRow.removeFromLeft(gap);
             playlistButtons[i]->setBounds(row.removeFromLeft(size));
+            labels[i]->setBounds(labelRow.removeFromLeft(size));
         }
     }
 
     void createPlaylists()
     {
+        // Save new playlist button
+        auto newPlaylistButton = std::move(playlistButtons[0]);
         playlistButtons.clear();
+        playlistButtons.push_back(std::move(newPlaylistButton));
+
+        labels.clear();
+
+        //Add the label for new playlist button 
+        auto label = std::make_unique<juce::Label>("Label","New Playlist");
+        addAndMakeVisible(label.get());
+        labels.push_back(std::move(label));
+
+        // Add the other buttons and labels 
         for(int i = 0;i<library.getNumChildren();i++)
         {
             std::unique_ptr<juce::ImageButton> playlistButton = std::make_unique<juce::ImageButton>("Button");
@@ -82,13 +100,18 @@ public:
 
             addAndMakeVisible(playlistButton.get());
             playlistButtons.push_back(std::move(playlistButton));
+
+            juce::String labelText = playlistVT.getPropertyAsValue(ID::PlaylistName, nullptr).toString();
+            auto label = std::make_unique<juce::Label>("Label",labelText);
+            addAndMakeVisible(label.get());
+            labels.push_back(std::move(label));
         }
 
         resized();
     }
 
 private:
-    std::vector<std::unique_ptr<juce::ImageButton>> playlistButtons;
-    juce::ImageButton newPlaylistButton;
+    std::vector<std::unique_ptr<juce::ImageButton>> playlistButtons; 
+    std::vector<std::unique_ptr<juce::Label>> labels;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ListMenu)
 };
